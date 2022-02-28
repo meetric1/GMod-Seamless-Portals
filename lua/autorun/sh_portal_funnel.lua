09fdeedf57980bf3ac382653241f591880d3c457
Wasabi_Thumbs <29989488+WasabiThumb@users.noreply.github.com>
2022-02-28 01:21:17 +0000

    Change comment style
    GitHub is not a fan of comments notated with //, and -- is used elsewhere in the addon.

--- lua/autorun/sh_portal_funnel.lua
+++ lua/autorun/sh_portal_funnel.lua
@@ -1,16 +1,16 @@
 
-// possible todo: make these convars?
+-- possible todo: make these convars?
 
-// what percentage of the speed to convert into funneling
+-- what percentage of the speed to convert into funneling
 local attractionPower = 0.2
 local attractionPowerStrafing = 0.02
-// at what distance to stop looking for portals to funnel into
+-- at what distance to stop looking for portals to funnel into
 local attractionRadius = 480
-// if the current direction is this much offset from the inward direction
-// of the portal, then do not funnel. We wouldn't want to funnel a player
-// into a portal that they are not expressing any intent to enter.
+-- if the current direction is this much offset from the inward direction
+-- of the portal, then do not funnel. We wouldn't want to funnel a player
+-- into a portal that they are not expressing any intent to enter.
 local angleRange = math.pi / 4
-// currently uncapped. the option is there
+-- currently uncapped. the option is there
 local speedCap = -1
 hook.Add("Move", "sp_portal_funnel", function(ply, move)
 
@@ -18,9 +18,9 @@ hook.Add("Move", "sp_portal_funnel", function(ply, move)
 	local threshold = math.pow(ply:GetRunSpeed(), 2)
 	local magSqr = vel:LengthSqr()
 	local portal = NULL
-	// Only if the player is traveling at least their run speed.
-	// At lower speeds, players aren't really going to need funneling.
-	// This also allows us to elegantly sidestep some issues.
+	-- Only if the player is traveling at least their run speed.
+	-- At lower speeds, players aren't really going to need funneling.
+	-- This also allows us to elegantly sidestep some issues.
 	if (magSqr >= threshold) then
 		local found = ents.FindInSphere(ply:GetPos(), attractionRadius)
 		for _,ent in ipairs(found) do
@@ -35,8 +35,8 @@ hook.Add("Move", "sp_portal_funnel", function(ply, move)
 		local inward = -portal:GetUp()
 		local angle = math.acos((vel.x * inward.x + vel.y * inward.y + vel.z * inward.z) / mag)
 		if (math.abs(angle) <= angleRange) then
-			// Crucially, we are not adding any energy here. Magnitude stays
-			// the same before and after.
+			-- Crucially, we are not adding any energy here. Magnitude stays
+			-- the same before and after.
 			local towards = (portal:GetPos() - ply:GetPos()):GetNormalized() * mag
 			local power = attractionPower
 			if (move:GetSideSpeed() > 0) then power = attractionPowerStrafing end
