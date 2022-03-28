98690652d3bf88d514616482a22b61f9516c1715
Deyan Dobromirov <dvdvideo123@gmail.com>
2022-03-28 21:06:50 +0300

    Trace detour optimization

--- lua/autorun/sh_detours.lua
+++ lua/autorun/sh_detours.lua
@@ -3,7 +3,7 @@ AddCSLuaFile()
 
 -- bullet detour
 hook.Add("EntityFireBullets", "seamless_portal_detour_bullet", function(entity, data)
-    if !SeamlessPortals or SeamlessPortals.PortalIndex < 1 then return end
+	if !SeamlessPortals or SeamlessPortals.PortalIndex < 1 then return end
 	local tr = SeamlessPortals.TraceLine({start = data.Src, endpos = data.Src + data.Dir * data.Distance, filter = entity})
 	local hitPortal = tr.Entity
 	if !hitPortal:IsValid() then return end
@@ -24,8 +24,8 @@ end)
 -- effect detour (Thanks to WasabiThumb)
 local oldUtilEffect = util.Effect
 local function effect(name, b, c, d)
-     if SeamlessPortals.PortalIndex > 0 and (name == "phys_freeze" or name == "phys_unfreeze") then return end
-     oldUtilEffect(name, b, c, d)
+	 if SeamlessPortals.PortalIndex > 0 and (name == "phys_freeze" or name == "phys_unfreeze") then return end
+	 oldUtilEffect(name, b, c, d)
 end
 util.Effect = effect
 
@@ -34,26 +34,28 @@ SeamlessPortals = SeamlessPortals or {}
 SeamlessPortals.TraceLine = SeamlessPortals.TraceLine or util.TraceLine
 local function editedTraceLine(data)
 	local tr = SeamlessPortals.TraceLine(data)
-	if tr.Entity:IsValid() and tr.Entity:GetClass() == "seamless_portal" and tr.Entity:ExitPortal() and tr.Entity:ExitPortal():IsValid() then
-		local hitPortal = tr.Entity
-		if tr.HitNormal:Dot(hitPortal:GetUp()) > 0 then
-			local editeddata = table.Copy(data)
-			editeddata.start = SeamlessPortals.TransformPortal(hitPortal, hitPortal:ExitPortal(), tr.HitPos)
-			editeddata.endpos = SeamlessPortals.TransformPortal(hitPortal, hitPortal:ExitPortal(), data.endpos)
-			-- filter the exit portal from being hit by the ray
-			if IsEntity(data.filter) then
-				editeddata.filter = {data.filter, hitPortal:ExitPortal()}
-			else
-				if istable(editeddata.filter) then
-					table.insert(editeddata.filter, hitPortal:ExitPortal())
-				else
-					editeddata.filter = hitPortal:ExitPortal()
-				end
-			end
-			return SeamlessPortals.TraceLine(editeddata)
-		end
+	if data["disable_seamless"] then return tr end
+	local trEnt = tr.Entity -- trace portal entity
+	if !trEnt or !trEnt:IsValid() then return tr end
+	if trEnt:GetClass() ~= "seamless_portal" then return tr end
+	local exEnt = trEnt:ExitPortal()
+	if !exEnt or !exEnt:IsValid() then return tr end
+	if tr.HitNormal:Dot(trEnt:GetUp()) > 0 then
+	local editeddata = table.Copy(data)
+	editeddata.start = SeamlessPortals.TransformPortal(trEnt, exEnt, tr.HitPos)
+	editeddata.endpos = SeamlessPortals.TransformPortal(trEnt, exEnt, data.endpos)
+	-- filter the exit portal from being hit by the ray
+	if IsEntity(data.filter) then
+		editeddata.filter = {data.filter, exEnt}
+	else
+	  if istable(editeddata.filter) then
+			table.insert(editeddata.filter, exEnt)
+	  else
+			editeddata.filter = exEnt
+	  end
 	end
-	return tr
+		return SeamlessPortals.TraceLine(editeddata)
+	end
 end
 
 -- use original traceline if there are no portals
@@ -69,16 +71,16 @@ if SERVER then return end
 
 -- sound detour
 hook.Add("EntityEmitSound", "seamless_portals_detour_sound", function(t)
-    if !SeamlessPortals or SeamlessPortals.PortalIndex < 1 then return end
+	if !SeamlessPortals or SeamlessPortals.PortalIndex < 1 then return end
 	for k, v in ipairs(ents.FindByClass("seamless_portal")) do
-        if !v.ExitPortal or !v:ExitPortal() or !v:ExitPortal():IsValid() then continue end
-        if !t.Pos or !t.Entity or t.Entity == NULL then continue end
-        if t.Pos:DistToSqr(v:GetPos()) < 50000 * v:ExitPortal():GetExitSize()[1] and (t.Pos - v:GetPos()):Dot(v:GetUp()) > 0 then
-            local newPos, _ = SeamlessPortals.TransformPortal(v, v:ExitPortal(), t.Pos, Angle())
-            local oldPos = t.Entity:GetPos() or Vector()
-            t.Entity:SetPos(newPos)
-            EmitSound(t.SoundName, newPos, t.Entity:EntIndex(), t.Channel, t.Volume, t.SoundLevel, t.Flags, t.Pitch, t.DSP)
-            t.Entity:SetPos(oldPos)
-        end
+		if !v.ExitPortal or !v:ExitPortal() or !v:ExitPortal():IsValid() then continue end
+		if !t.Pos or !t.Entity or t.Entity == NULL then continue end
+		if t.Pos:DistToSqr(v:GetPos()) < 50000 * v:ExitPortal():GetExitSize()[1] and (t.Pos - v:GetPos()):Dot(v:GetUp()) > 0 then
+			local newPos, _ = SeamlessPortals.TransformPortal(v, v:ExitPortal(), t.Pos, Angle())
+			local oldPos = t.Entity:GetPos() or Vector()
+			t.Entity:SetPos(newPos)
+			EmitSound(t.SoundName, newPos, t.Entity:EntIndex(), t.Channel, t.Volume, t.SoundLevel, t.Flags, t.Pitch, t.DSP)
+			t.Entity:SetPos(oldPos)
+		end
 	end
 end)
