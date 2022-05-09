b73d19823bfb02643ef8ebd48d9f9792dc26d929
Reginald 'M' Halifrax <iamtristan99@yahoo.com>
2022-05-08 22:04:51 -0400

    Update to modern api, support new features.

--- lua/entities/gmod_wire_expression2/core/custom/cl_seamlessportals.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_seamlessportals.lua
@@ -1,4 +1,5 @@
-E2Helper.Descriptions["createPortalPair(vava)"] = "Creates a link pair of portals and returns them in an entity array."
+E2Helper.Descriptions["createPortalPair(vava)"] = "Creates a link pair of portals and returns them in an entity array.\nIf one of the portals are deleted, its counterpart is also deleted."
+E2Helper.Descriptions["createPortal(va)"] = "Creates and returns an unlinked portal."
 E2Helper.Descriptions["getPortalExit(e:)"] = "Returns the exit portal entity that this portal is paired with."
 E2Helper.Descriptions["getPortalSize(e:)"] = "Returns the size of the portal as a vector."
 E2Helper.Descriptions["getPortalExitSize(e:)"] = "Returns the size of the portal this is paired with as a vector."
@@ -6,4 +7,7 @@ E2Helper.Descriptions["setPortalSize(e:xv2)"] = "Sets the size of the portal, cl
 E2Helper.Descriptions["setPortalSize(e:nn)"] = E2Helper.Descriptions["setPortalSize(e:xv2)"]
 E2Helper.Descriptions["setPortalExitSize(e:xv2)"] = "Sets the size of the portal this is paired with.\nReturns 0 on fail, 1 on success."
 E2Helper.Descriptions["setPortalExitSize(e:nn)"] = E2Helper.Descriptions["setPortalExitSize(e:xv2)"]
-E2Helper.Descriptions["removePortalPair(e:)"] = "Removes a linked pair of portals, recommended to put in a last() function.\nReturns 0 on fail, 1 on success."
+E2Helper.Descriptions["setPortalSides(e:n)"] = "Sets the portal sides to the given number clamped between 3 to 100.\nReturns 0 on fail, 1 on success."
+E2Helper.Descriptions["setPortalExitSides(e:n)"] = E2Helper.Descriptions["setPortalSides(e:n)"]
+E2Helper.Descriptions["linkPortal(e:e)"] = "Links the given portal to another portal.\nReturns 0 on fail, 1 on success."
+E2Helper.Descriptions["removePortal(e:)"] = "Removes a portal, recommended to put in a last() function.\nReturns 0 on fail, 1 on success."
--- lua/entities/gmod_wire_expression2/core/custom/seamlessportals.lua
+++ lua/entities/gmod_wire_expression2/core/custom/seamlessportals.lua
@@ -1,5 +1,12 @@
 if (E2Lib==nil) then return end
 
+--[[
+Some notes:
+	Only functions such as linkPortal() checks for CPPI ownership on portal pairs as we assume that once paired, ownership is assumed.
+
+	Requesting information from a portal and/or its pair does not require authorization as we aren't modifying anything.
+]]
+
 E2Lib.RegisterExtension("SeamlessPortalsCore", true)
 
 __e2setcost( 35 )
@@ -18,10 +25,7 @@ e2function array createPortalPair(vector pos1, angle ang1, vector pos2, angle an
 	portal2:SetAngles(Angle(ang2[1], ang2[2], ang2[3]) + Angle(90, 0, 0))
 	portal2.PORTAL_REMOVE_EXIT = true
 
-	portal1.PORTAL_EXIT = portal2
-	portal2.PORTAL_EXIT = portal1
-	portal1:SetPortalExit(portal2)
-	portal2:SetPortalExit(portal1)
+	portal1:LinkPortal(portal2)
 
 	if CPPI then
 		portal1:CPPISetOwner(self.player)
@@ -31,12 +35,26 @@ e2function array createPortalPair(vector pos1, angle ang1, vector pos2, angle an
 	return {portal1, portal2}
 end
 
+e2function entity createPortal(vector pos, angle ang)
+	local portal = ents.Create("seamless_portal")
+
+	portal:Spawn()
+	portal:SetPos(Vector(pos[1],pos[2],pos[3]))
+	portal:SetAngles(Angle(ang[1], ang[2], ang[3]) + Angle(90, 0, 0))
+
+	if CPPI then
+		portal:CPPISetOwner(self.player)
+	end
+
+	return portal
+end
+
 __e2setcost( 10 )
 
 e2function entity entity:getPortalExit()
 	if not IsValid(this) or not this:GetClass() == "seamless_portal" then return nil end
-	if not IsValid(this.PORTAL_EXIT) then return nil end
-	return this.PORTAL_EXIT
+	if not IsValid(this:GetExitPortal()) then return nil end
+	return this:GetExitPortal()
 end
 
 e2function vector entity:getPortalSize()
@@ -46,11 +64,20 @@ end
 
 e2function vector entity:getPortalExitSize()
 	if not IsValid(this) or not this:GetClass() == "seamless_portal" then return nil end
-	if not IsValid(this.PORTAL_EXIT) then return nil end
-	return this.PORTAL_EXIT:GetExitSize()
+	if not IsValid(this:GetExitPortal()) then return nil end
+	return this:GetExitPortal():GetExitSize()
 end
 
 __e2setcost( 25 )
+
+e2function number entity:linkPortal(entity targetPortal)
+	if (not IsValid(this) or not this:GetClass() == "seamless_portal") or (not IsValid(targetPortal) or not targetPortal:GetClass() == "seemless_portal") then return 0 end
+	if CPPI == nil or ((this:CPPIGetOwner() == self.player or E2Lib.isFriend(this:CPPIGetOwner(), self.player)) and (targetPortal:CPPIGetOwner() == self.player or E2Lib.isFriend(targetPortal:CPPIGetOwner(), self.player))) then
+		this:LinkPortal(targetPortal)
+		return 1
+	end
+	return 0
+end
 
 e2function number entity:setPortalSize(vector2 size)
 	if not IsValid(this) or not this:GetClass() == "seamless_portal" then return 0 end 
@@ -58,6 +85,7 @@ e2function number entity:setPortalSize(vector2 size)
 		this:SetExitSize(Vector(math.Clamp(size[2],0.01,10), math.Clamp(size[1],0.01,10), 1))
 		return 1
 	end
+	return 0
 end
 
 e2function number entity:setPortalSize(number x, number y)
@@ -66,25 +94,46 @@ e2function number entity:setPortalSize(number x, number y)
 		this:SetExitSize(Vector(math.Clamp(y,0.01,10), math.Clamp(x,0.01,10), 1))
 		return 1
 	end
+	return 0
 end
 
 e2function number entity:setPortalExitSize(vector2 size)
-	if (not (IsValid(this) or not this:GetClass() == "seamless_portal") and IsValid(this.PORTAL_EXIT)) then return 0 end
-	if CPPI == nil or (this.PORTAL_EXIT:CPPIGetOwner() == self.player or E2Lib.isFriend(this.PORTAL_EXIT:CPPIGetOwner(), self.player)) then
-		this.PORTAL_EXIT:SetExitSize(Vector(math.Clamp(size[2],0.01,10), math.Clamp(size[1],0.01,10), 1))
+	if (not (IsValid(this) or not this:GetClass() == "seamless_portal") and IsValid(this:GetExitPortal())) then return 0 end
+	if CPPI == nil or (this:GetExitPortal():CPPIGetOwner() == self.player or E2Lib.isFriend(this:GetExitPortal():CPPIGetOwner(), self.player)) then
+		this:GetExitPortal():SetExitSize(Vector(math.Clamp(size[2],0.01,10), math.Clamp(size[1],0.01,10), 1))
 		return 1
 	end
+	return 0
 end
 
 e2function number entity:setPortalExitSize(number x, number y)
-	if (not (IsValid(this) or not this:GetClass() == "seamless_portal") and IsValid(this.PORTAL_EXIT)) then return 0 end
-	if CPPI == nil or (this.PORTAL_EXIT:CPPIGetOwner() == self.player or E2Lib.isFriend(this.PORTAL_EXIT:CPPIGetOwner(), self.player)) then
-		this.PORTAL_EXIT:SetExitSize(Vector(math.Clamp(y,0.01,10), math.Clamp(x,0.01,10), 1))
+	if (not (IsValid(this) or not this:GetClass() == "seamless_portal") and IsValid(this:GetExitPortal())) then return 0 end
+	if CPPI == nil or (this:GetExitPortal():CPPIGetOwner() == self.player or E2Lib.isFriend(this:GetExitPortal():CPPIGetOwner(), self.player)) then
+		this:GetExitPortal():SetExitSize(Vector(math.Clamp(y,0.01,10), math.Clamp(x,0.01,10), 1))
 		return 1
 	end
+	return 0
 end
 
-e2function number entity:removePortalPair()
+e2function number entity:setPortalSides(number sides)
+	if not IsValid(this) or not this:GetClass() == "seamless_portal" then return 0 end
+	if CPPI == nil or (this:CPPIGetOwner() == self.player or E2Lib.isFriend(this:CPPIGetOwner(), self.player)) then
+		this:SetSides(sides) -- Function already internally clamps the value, no need to do it on our end.
+		return 1
+	end
+	return 0
+end
+
+e2function number entity:setPortalExitSides(number sides)
+	if (not (IsValid(this) or not this:GetClass() == "seamless_portal") and IsValid(this:GetExitPortal())) then return 0 end
+	if CPPI == nil or (this:GetExitPortal():CPPIGetOwner() == self.player or E2Lib.isFriend(this:GetExitPortal():CPPIGetOwner(), self.player)) then
+		this:GetExitPortal():SetSides(sides)
+		return 1
+	end
+	return 0
+end
+
+e2function number entity:removePortal()
 	if not IsValid(this) or not this:GetClass() == "seamless_portal" then return 0 end
 	if CPPI == nil or (this:CPPIGetOwner() == self.player or E2Lib.isFriend(this:CPPIGetOwner(), self.player)) then
 		SafeRemoveEntity(this)
